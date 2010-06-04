cad482d843f87066eb82720cd0cf96d745a15a3d
magnificop <magnificop@82c4f2bc-2dcd-11df-a735-6ff41f51f3aa>
2010-06-04 15:42:34 +0000

    Łączenie z klientem, uwierzytelnianie.

--- prog/Klient/Klient/Klient/SelectProfile.Designer.cs
+++ prog/Klient/Klient/Klient/SelectProfile.Designer.cs
@@ -61,9 +61,10 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Numer:";
+            this.label1.Text = "Login:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
